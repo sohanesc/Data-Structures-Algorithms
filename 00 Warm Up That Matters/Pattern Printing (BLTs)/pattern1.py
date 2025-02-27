e7f63fe8c687b9f270196efb26802847b631ba52
sohanxsahoo <sohanxsahoo@gmail.com>
2025-02-27 12:10:29 +0530

    Remove pattern printing example scripts

--- 00 Warm Up That Matters/Pattern Printing (BLTs)/pattern1.py
+++ 00 Warm Up That Matters/Pattern Printing (BLTs)/pattern1.py
@@ -4,6 +4,8 @@ n = int(input("Enter the number of rows: "))
 for i in range(n):
     # Loop to print ❤️ in each column
     for j in range(n):
-        print("❤️", end=" ")
+        print("1", end=" ")
     # Print a new line after each row
     print()
+
+# TC: O(n^2)
