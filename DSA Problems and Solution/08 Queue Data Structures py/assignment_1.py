1f540c02209250d159eed119f0db17dce85cb69d
sohanxsahoo <sohanxsahoo@gmail.com>
2025-03-10 19:59:33 +0530

    Queue initialise

--- DSA Problems and Solution/08 Queue Data Structures py/assignment_1.py
+++ DSA Problems and Solution/08 Queue Data Structures py/assignment_1.py
@@ -9,3 +9,45 @@ Assignment-12: Queue using list
 7. In Queue class, define size() method to return size of the queue that is number of items present in the queue.
 """
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def enqueue(self, data):
+        self.items.append(data)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Queue is Empty")
+        
+    def get_front(self):
+        if not self.is_empty():
+            return self.items[0]
+        else:
+            raise IndexError("Queue is Empty")
+        
+    def get_rear(self):
+        if not self.is_empty():
+            return self.items[-1]
+        else:
+            raise IndexError("Queue is Empty")
+        
+    def size(self):
+        return len(self.items)
+    
+s1 = Queue()
+s1.enqueue(10)
+s1.enqueue(20)
+s1.enqueue(30)
+print("Size of Queue: ", s1.size())
+print("Front of Queue: ", s1.get_front())
+print("Rear of Queue: ", s1.get_rear())
+print("Removed element: ", s1.dequeue())
+print("Size of Queue: ", s1.size())
+print("Front of Queue: ", s1.get_front())
+print("Rear of Queue: ", s1.get_rear())
